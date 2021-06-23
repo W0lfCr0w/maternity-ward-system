d378ca4f0d6ed727002d5e21d6301ed38af11719
W0lfCr0w <76149797+W0lfCr0w@users.noreply.github.com>
2021-06-23 10:03:06 +0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace maternity_ward_system
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
