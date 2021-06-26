28cf3d86b70fdef7ca04a70e4677eeed855172fd
W0lfCr0w <76149797+W0lfCr0w@users.noreply.github.com>
2021-06-26 18:42:47 +0300

    Created Supervisor Cleaner class + minor changes to PosionCleaner class

---
+++ SupervisorCleaner.cs
@@ -0,0 +1,32 @@
+namespace maternity_ward_system
+{
+    public class SupervisorCleaner : Employee, IDecisionMaker
+    {
+        public double HourlyPay{get; private set;}
+        public string UniqueJobDescripition{get; private set;}
+        public int MinimumMonthlyHours{get; private set;}
+        public SupervisorCleaner(string fname, string lname, string id, int age, double hours)
+        {
+            FirstName = fname;
+            LastName = lname;
+            ID = id;
+            Age = age;
+            this.workInformation = new WorkingHours(hours);
+            HourlyPay = BasePay;
+            UniqueJobDescripition = "Supervises over all cleaners";
+            MinimumMonthlyHours = 50;
+        }
+        public SupervisorCleaner(string fname, string lname, string id, int age) :this(fname, lname, id, age, 0){}
+        public double GetDecisionMakerBonusPay()
+        {
+            if(this.workInformation.HoursWorked > MinimumMonthlyHours)
+                return 200 * (1.5 * HourlyPay);
+            else
+                return 0;
+        }
+        public override double EndOfMonthSalary()
+        {
+            return this.GetDecisionMakerBonusPay();
+        }
+    }
+}
