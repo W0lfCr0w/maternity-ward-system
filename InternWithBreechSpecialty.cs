07a43dd8195d4e8fbbb435c23460426f9a6065f9
W0lfCr0w <76149797+W0lfCr0w@users.noreply.github.com>
2021-06-26 21:38:52 +0300

    Changed the Breech Intern to derive from regular intern

--- InternWithBreechSpecialty.cs
+++ InternWithBreechSpecialty.cs
@@ -1,15 +1,11 @@
 namespace maternity_ward_system
 {
-    public class InternWithBreechSpecialty: Employee, IExpertEmployee
+    public class InternWithBreechSpecialty: Intern, IExpertEmployee
     {
         public double ExpertHourlyPay{get; private set;}
         public InternWithBreechSpecialty(string fname, string lname, string id, int age, double hours)
+            :base(fname, lname, id, age, hours)
         {
-            FirstName = fname;
-            LastName = lname;
-            ID = id;
-            Age = age;
-            this.workInformation = new WorkingHours(hours);
             ExpertHourlyPay = BasePay * 1.3 ;
         }
         public InternWithBreechSpecialty(string fname, string lname, string id, int age) :this(fname, lname, id, age, 0){}
