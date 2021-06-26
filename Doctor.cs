b41ba07905f69a44e150e0f403c8d064f269de3d
W0lfCr0w <76149797+W0lfCr0w@users.noreply.github.com>
2021-06-26 20:27:10 +0300

    Created Doctor class

---
+++ Doctor.cs
@@ -0,0 +1,27 @@
+namespace maternity_ward_system
+{
+    public class Doctor: Employee, ISeniorEmployee
+    {
+        public double HourlyPay{get; private set;}
+        public void SeniorEmployeeRaise()
+        {
+            HourlyPay = HourlyPay * 1.05;
+        }
+        public Doctor(string fname, string lname, string id, int age, double hours)
+        {
+            FirstName = fname;
+            LastName = lname;
+            ID = id;
+            Age = age;
+            this.workInformation = new WorkingHours(hours);
+            HourlyPay = BasePay;
+            SeniorEmployeeRaise();
+        }
+        public Doctor(string fname, string lname, string id, int age) :this(fname, lname, id, age, 0){}
+        public override double EndOfMonthSalary()
+        {
+            return HourlyPay * this.workInformation.HoursWorked;
+        }
+        
+    }
+}
