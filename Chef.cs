c3849c1b3fc8083426b6a88b1a15382340b0ad71
W0lfCr0w <76149797+W0lfCr0w@users.noreply.github.com>
2021-06-26 20:19:43 +0300

    minor addition to Chef class

--- Chef.cs
+++ Chef.cs
@@ -20,6 +20,8 @@ namespace maternity_ward_system
             HourlyPay = BasePay;
             SeniorEmployeeRaise();
             ExpertHourlyPay = HourlyPay * 1.3;
+            UniqueJobDescripition = "The Chef of the kitchen makes choices about the food being served";
+            MinimumMonthlyHours = 50;
         }
         public Chef(string fname, string lname, string id, int age) :this(fname, lname, id, age, 0){}
         public double GetDecisionMakerBonusPay()
