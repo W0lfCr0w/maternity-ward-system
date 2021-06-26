ad2837e137095cf57231387506e65864de2ce811
W0lfCr0w <76149797+W0lfCr0w@users.noreply.github.com>
2021-06-26 20:51:12 +0300

    Created SeniorDoctor class

---
+++ SeniorDoctor.cs
@@ -0,0 +1,30 @@
+namespace maternity_ward_system
+{
+    public class SeniorDoctor: Doctor, IDecisionMaker
+    {
+        public string UniqueJobDescripition{get; protected set;}
+        public int MinimumMonthlyHours{get; protected set;}
+        public SeniorDoctor(string fname, string lname, string id, int age, double hours) :base(fname, lname, id, age, hours)
+        {
+            FirstName = fname;
+            LastName = lname;
+            ID = id;
+            Age = age;
+            this.workInformation = new WorkingHours(hours);
+            HourlyPay = BasePay;
+            SeniorEmployeeRaise();
+        }
+        public SeniorDoctor(string fname, string lname, string id, int age) :this(fname, lname, id, age, 0){}
+        public double GetDecisionMakerBonusPay()
+        {
+            if(this.workInformation.HoursWorked > MinimumMonthlyHours)
+                return 200 * (1.5 * HourlyPay);
+            else
+                return 0;
+        }
+        public override double EndOfMonthSalary()
+        {
+            return base.EndOfMonthSalary() + this.GetDecisionMakerBonusPay();
+        }
+    }
+}
