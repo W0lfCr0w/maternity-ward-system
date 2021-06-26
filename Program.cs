7ba954ab598196e33c30e80bdf81b343575f997a
W0lfCr0w <76149797+W0lfCr0w@users.noreply.github.com>
2021-06-26 12:51:57 +0300

    Changed console type and cleaned the Main func for starting a clean project

--- Program.cs
+++ Program.cs
@@ -4,9 +4,11 @@ namespace maternity_ward_system
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+
         }
     }
 }
