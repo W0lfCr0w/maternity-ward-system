a761cf0e9d497a3c890c2692bb9c009018ff79ea
W0lfCr0w <76149797+W0lfCr0w@users.noreply.github.com>
2021-06-26 21:34:14 +0300

    Created Head of ward class

---
+++ HeadOfMaternityWard.cs
@@ -0,0 +1,20 @@
+namespace maternity_ward_system
+{
+    public class HeadOfMaternityWard: ViceHeadOfMaternitiyWard, IRiskFactorEmployee
+    {
+        public double RiskFactorBonus{get; private set;}
+        public HeadOfMaternityWard(string fname, string lname, string id, int age, double hours, double managerSalaray)
+            :base(fname, lname, id, age, hours, managerSalaray)
+            {
+                RiskFactorBonus = 2.0;
+            }
+        public HeadOfMaternityWard(string fname, string lname, string id, int age, double managerSalaray)
+            :this(fname, lname, id, age, 0, managerSalaray){}
+        
+        public override double EndOfMonthSalary()
+        {
+            return base.EndOfMonthSalary() * RiskFactorBonus;
+        }
+        
+    }
+}
